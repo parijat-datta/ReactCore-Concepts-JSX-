f7782ec729f9652b9017cce1492d508f432c46af
parijat-datta <parijatdutt@gmail.com>
2020-08-22 00:38:55 +0600

    Pretty Basics

--- src/App.js
+++ src/App.js
@@ -2,25 +2,88 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
+
+
+  const style={
+
+  color:'red',
+  fontWeight:'700',
+  backgroundColor:'yellow'
+
+  }
+
+  
+  
 function App() {
+  
+  const players=["Messi","C.Ronaldo"];
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+       
+         
+        <h3>I am a React Hero</h3>
+         <Person></Person>
+          <Footballer name={players[0]} nationality="Argentina" ></Footballer>      
+          <Footballer name={players[1]} nationality="Portugal"></Footballer>
+
+         
+          </header>
+          
+     
+  </div>
   );
+}
+
+
+function Person(){
+
+const personStyle={
+color:'orange',
+border:'2px solid yellow',
+marginBottom:'10px',
+padding:"10px 20px"
+
+}
+
+return(
+<div style={personStyle}>
+
+<h1>Shakib Al Hasan</h1>
+<h3>Hero of the Year</h3>
+
+</div>
+
+)
+
+
+}
+
+function Footballer(props){
+  const footballStyle={
+   
+    border:"2px solid gold",
+    padding:"10px 15px",
+    borderRadius:"10px"
+
+
+  }
+console.log(props)
+
+return (
+
+<div style={footballStyle}>
+
+<h1>The Best: {props.name} </h1>
+<h3>Nationality: {props.nationality} </h3>
+
+</div>
+
+)
+
+
 }
 
 export default App;
